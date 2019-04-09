3087af00eaa167096c9b1415af6f8d56de49b7f2
Alexis Montagne <alexis.montagne@gmail.com>
2019-04-09 14:06:58 -0400

    *: Add benchmark to compare this logger and logrus

--- logger_test.go
+++ logger_test.go
@@ -5,19 +5,22 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	"github.com/upfluence/log/record"
 	"github.com/upfluence/log/sink/leveled"
 	"github.com/upfluence/log/sink/writer"
 )
 
-func logBenchmark(b *testing.B, fn func(Logger, int), opts []LoggerOption) {
-	var l = NewLogger(opts...)
+func logBenchmark(b *testing.B, fn func(Logger, int), lb loggerBench) {
+	var l = lb.logger()
 
 	b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		fn(l, i)
-	}
+	b.Run(lb.bname(), func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			fn(l, i)
+		}
+	})
 }
 
 type staticFormatter struct{}
@@ -28,33 +31,91 @@ type staticSink struct{}
 
 func (staticSink) Log(record.Record) error { return nil }
 
-type loggerBench struct {
+type loggerBench interface {
+	bname() string
+	logger() Logger
+}
+
+type loggerBenchUpf struct {
 	name string
 	opts []LoggerOption
 }
 
+func (b loggerBenchUpf) bname() string  { return "upfluence/log/" + b.name }
+func (b loggerBenchUpf) logger() Logger { return NewLogger(b.opts...) }
+
+type loggerBenchLogrus struct {
+	name string
+
+	l logrus.FieldLogger
+}
+
+func (b loggerBenchLogrus) bname() string  { return "logrus/" + b.name }
+func (b loggerBenchLogrus) logger() Logger { return &logrusAdapter{lr: b.l} }
+
+type logrusAdapter struct {
+	Logger
+
+	lr logrus.FieldLogger
+}
+
+func (la *logrusAdapter) Info(vs ...interface{}) { la.lr.Info(vs...) }
+func (la *logrusAdapter) Infof(fmt string, vs ...interface{}) {
+	la.lr.Infof(fmt, vs...)
+}
+func (la *logrusAdapter) WithField(f record.Field) SugaredLogger {
+	return &logrusAdapter{lr: la.lr.WithField(f.GetKey(), f.GetValue())}
+}
+
 var benches = []loggerBench{
-	loggerBench{
+	loggerBenchUpf{
 		name: "standard with stack trace",
 		opts: []LoggerOption{WithSink(writer.NewStandardSink(ioutil.Discard))},
 	},
-	loggerBench{
+	loggerBenchLogrus{
+		name: "standard Text with no stacktrace",
+		l: &logrus.Logger{
+			Out:       ioutil.Discard,
+			Formatter: new(logrus.TextFormatter),
+			Hooks:     make(logrus.LevelHooks),
+			Level:     logrus.InfoLevel,
+		},
+	},
+	loggerBenchLogrus{
+		name: "standard JSON with no stacktrace",
+		l: &logrus.Logger{
+			Out:       ioutil.Discard,
+			Formatter: new(logrus.JSONFormatter),
+			Hooks:     make(logrus.LevelHooks),
+			Level:     logrus.InfoLevel,
+		},
+	},
+	loggerBenchLogrus{
+		name: "leved",
+		l: &logrus.Logger{
+			Out:       ioutil.Discard,
+			Formatter: new(logrus.JSONFormatter),
+			Hooks:     make(logrus.LevelHooks),
+			Level:     logrus.WarnLevel,
+		},
+	},
+	loggerBenchUpf{
 		name: "standard with no stack trace",
 		opts: []LoggerOption{
 			WithSink(writer.NewSink(writer.NewFastFormatter(), ioutil.Discard)),
 		},
 	},
-	loggerBench{
+	loggerBenchUpf{
 		name: "static formatter",
 		opts: []LoggerOption{
 			WithSink(writer.NewSink(staticFormatter{}, ioutil.Discard)),
 		},
 	},
-	loggerBench{
+	loggerBenchUpf{
 		name: "static sink",
 		opts: []LoggerOption{WithSink(staticSink{})},
 	},
-	loggerBench{
+	loggerBenchUpf{
 		name: "leveled",
 		opts: []LoggerOption{
 			WithSink(
@@ -66,24 +127,32 @@ var benches = []loggerBench{
 
 func BenchmarkFixedString(b *testing.B) {
 	for _, bench := range benches {
-		b.Run(bench.name, func(b *testing.B) {
-			logBenchmark(
-				b,
-				func(l Logger, _ int) { l.Info("foo bar") },
-				bench.opts,
-			)
-		})
+		logBenchmark(
+			b,
+			func(l Logger, _ int) { l.Info("foo bar") },
+			bench,
+		)
 	}
 }
 
 func BenchmarkFmtString(b *testing.B) {
 	for _, bench := range benches {
-		b.Run(bench.name, func(b *testing.B) {
-			logBenchmark(
-				b,
-				func(l Logger, _ int) { l.Infof("foo bar %v", "foo") },
-				bench.opts,
-			)
-		})
+		logBenchmark(
+			b,
+			func(l Logger, _ int) { l.Infof("foo bar %v", "foo") },
+			bench,
+		)
+	}
+}
+
+func BenchmarkWithField(b *testing.B) {
+	f := Field("foo", "bar")
+
+	for _, bench := range benches {
+		logBenchmark(
+			b,
+			func(l Logger, _ int) { l.WithField(f).Info("foo bar") },
+			bench,
+		)
 	}
 }
